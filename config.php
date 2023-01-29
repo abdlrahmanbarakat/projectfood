b66de4fb1e932976fb68ba4617702c94a3158ca0
abdlrahmanbarakat <107102143+abdlrahmanbarakat@users.noreply.github.com>
2023-01-30 01:46:29 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$currency = '&#8377; '; 
-
-$db_username = 'root';
-$db_password = '';
-$db_name = 'test';
-$db_host = 'localhost';	
-						
-$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);						
