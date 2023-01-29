8a14385e4aeff8fce74c2dd98e79154dfefb9445
abdlrahmanbarakat <107102143+abdlrahmanbarakat@users.noreply.github.com>
2023-01-30 01:44:22 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-include_once("config.php");
+require("connect.php");
 $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-?>
 
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,39 +28,39 @@ $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_
 </div>' ?>
 
 <?php
-$results = $mysqli->query("SELECT product_code, product_name, price FROM products ORDER BY id ASC");
-if($results){ 
+$query = "SELECT product_code, product_name, price FROM products ORDER BY id ASC";
+$stmt = $pdo->prepare($query);
+$stmt->execute();
+
 $products_item = '<ul class="products">';
 
-while($obj = $results->fetch_object())
-{
-$products_item .= <<<EOT
-	<li class="product">
-	<form method="post" action="cart_update.php">
-	<div class="product-content"><h3>{$obj->product_name}</h3>
-	<div class="product-info">
-	Price {$currency}{$obj->price} 
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  $products_item .= <<<EOT
+  <li class="product">
+  <form method="post" action="cart_update.php">
+  <div class="product-content"><h3>{$row['product_name']}</h3>
+  <div class="product-info">
+  Price {$currency}{$row['price']} 
     <fieldset>
-	
-	<label>
-		<span>Quantity</span>
-		<input type="text" size="2" maxlength="2" name="product_qty" value="1" />
-	</label>
-	
-	</fieldset>
-	<input type="hidden" name="product_code" value="{$obj->product_code}" />
-	<input type="hidden" name="type" value="add" />
-	<input type="hidden" name="return_url" value="{$current_url}" />
-	<div align="center"><button type="submit" class="add_to_cart">Add</button></div>
+  
+  <label>
+    <span>Quantity</span>
+    <input type="text" size="2" maxlength="2" name="product_qty" value="1" />
+  </label>
+  
+  </fieldset>
+  <input type="hidden" name="product_code" value="{$row['product_code']}" />
+  <input type="hidden" name="type" value="add" />
+  <input type="hidden" name="return_url" value="{$current_url}" />
+  <div align="center"><button type="submit" class="add_to_cart">Add</button></div>
 
-	</div></div>
-	</form>
-	</li>
+  </div></div>
+  </form>
+  </li>
 EOT;
 }
 $products_item .= '</ul>';
 echo $products_item;
-}
 ?>
 </div>
 <footer class="footer">
